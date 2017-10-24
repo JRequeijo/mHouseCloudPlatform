8bd8fdb558bc8a587c0e2a0ea0d9ac3efb8244f3
Requeijo <Requeijo@MacBook-Pro-de-Requeijo.local>
2017-10-24 23:05:13 +0100

    Final Commit 1

--- mHouseCloudPlatform/settings.py
+++ mHouseCloudPlatform/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'kigb#@oive8-536vh&+qtg-pd*9pyweu-d4#rp1o4m3bp@%^ci'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'mhouseframework.eu-west-1.elasticbeanstalk.com']
 
